5c3e8f171d9244d651efb90435d2f8a768f8562c
Ntholi Nkhatho <ntholi@live.com>
2024-03-23 08:22:26 +0200

    Fix bug in login functionality

---
+++ prisma/migrations/20240322143542_add_physical_addressa_and_chief/migration.sql
@@ -0,0 +1,6 @@
+-- AlterTable
+ALTER TABLE "cases" ADD COLUMN     "disability" TEXT;
+
+-- AlterTable
+ALTER TABLE "personal_informations" ADD COLUMN     "physical_address" TEXT,
+ADD COLUMN     "village_chief" TEXT;
