7442ab893e162025477990ba6b36640f7e53f401
Ntholi Nkhatho <ntholi@live.com>
2024-03-29 07:53:36 +0200

    create PersonaInformation page

---
+++ src/app/personal-info/[id]/page.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import prisma from '@/lib/prisma';
+import FieldView from '@/components/FieldView';
+import { notFound } from 'next/navigation';
+import { Divider, Flex, Paper, Stack, Title } from '@mantine/core';
+import BackButton from '@/components/BackButton';
+
+type Props = { params: { id: string } };
+
+export default async function PersonaInformation({ params: { id } }: Props) {
+  const person = await prisma.personalInformation.findUnique({
+    where: {
+      id: id,
+    },
+  });
+
+  if (!person) return notFound();
+
+  return (
+    <Paper p={'xl'}>
+      <Flex justify={'space-between'} align={'center'}>
+        <Title order={1} fw={100} size={'3rem'}>
+          {person.firstName} {person.surname}
+        </Title>
+        <BackButton />
+      </Flex>
+      <Divider mt={'sm'} mb={'lg'} />
+      <Stack mt={'xl'}>
+        <FieldView label='National ID' value={person.nationalId} />
+        <FieldView label='National Type' value={person.nationalIdType} />
+        <FieldView label='Surname' value={person.surname} />
+        <FieldView label='Middle Name' value={person.middleName} />
+        <FieldView label='First Name' value={person.firstName} />
+        <FieldView label='Gender' value={person.gender} />
+        <FieldView label='Phone Number' value={person.phoneNumber} />
+        <FieldView label='Email' value={person.email} />
+        <FieldView label='Date of Birth' value={person.dateOfBirth} />
+        <FieldView label='Nationality' value={person.nationality} />
+        <FieldView label='Marital Status' value={person.maritalStatus} />
+        <FieldView label='Occupation' value={person.occupation} />
+        <FieldView label='Education Level' value={person.education} />
+        <FieldView label='Place of Birth' value={person.placeOfBirth} />
+        <FieldView label='Area Chief' value={person.areaChief} />
+        <FieldView label='Place of Residence' value={person.placeOfResidence} />
+        <FieldView label='Head Man' value={person.headMan} />
+        <FieldView label='Principle Chief' value={person.principalChief} />
+        <FieldView label='District' value={person.district} />
+        <FieldView label='Next Of Kin' value={person.nextOfKin} />
+        <FieldView label='Next Of Kin Phone' value={person.nextOfKinPhone} />
+      </Stack>
+    </Paper>
+  );
+}
--- src/components/PersonalInformationRow.tsx
+++ src/components/PersonalInformationRow.tsx
@@ -1,4 +1,6 @@
+import { Anchor } from '@mantine/core';
 import { PersonalInformation } from '@prisma/client';
+import Link from 'next/link';
 import React from 'react';
 
 type Props = {
@@ -9,17 +11,19 @@ export default function PersonalInformationRow({ items }: Props) {
   if (!items || items.length === 0) return null;
   if (items.length === 1)
     return (
-      <>
+      <Anchor component={Link} href={`/personal-info/${items[0].id}`}>
         {items[0].firstName} {items[0].surname}
-      </>
+      </Anchor>
     );
   return (
     <>
       {items.map((item, index) => (
-        <span key={index}>
-          {item.surname}
+        <React.Fragment key={item.id}>
+          <Anchor component={Link} href={`/personal-info/${item.id}`}>
+            {item.surname}
+          </Anchor>
           {index < items.length - 1 && ', '}
-        </span>
+        </React.Fragment>
       ))}
     </>
   );
